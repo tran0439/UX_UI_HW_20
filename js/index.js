7e23f2850f0d59ee32331b51f47b4fca53fff4be
tran0439 <63684667+tran0439@users.noreply.github.com>
2020-10-30 17:42:29 -0500

    secondary commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
     new WOW().init();
   });
+
+  $("#pop").on("click", function(e) {
+    e.preventDefault();
+    $('#the-modal').modal('toggle');
+ });
+
+ // optional
+ $('#blogCarousel').carousel({
+  interval: 5000
+});
